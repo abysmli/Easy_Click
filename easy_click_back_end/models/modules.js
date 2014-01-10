ffa1a6debff64159e099075df67491765b4b0d9f
abysmli <abysmli@gmail.com>
2014-01-10 03:38:06 +0100

    add new exchange module

---
+++ easy_click_back_end/models/modules.js
@@ -0,0 +1,47 @@
+var mongodb = require('./db');
+module.exports = Modules;
+
+function Modules(id, content, date) {
+  this.id = id;
+  this.content = content;
+  if (date) {
+    this.date = date;
+  } else {
+    this.date = new Date();
+  }
+};
+
+Modules.prototype.save = function save(db, callback) {
+  var modules = {
+    id: this.id,
+    content: this.content,
+    date: this.date,
+  };
+  db.collection('modules', function(err, collection) {
+    if (err) {
+      return callback(err);
+    }
+    collection.findAndModify({
+      id: modules.id
+    }, [['id', 1]], modules, {upsert:true}, function(err, modules) {
+      callback(err, modules);
+    });
+  });
+};
+
+Modules.getbyId = function getbyId(db, id, callback) {
+  db.collection('modules', function(err, collection) {
+    if (err) {
+      return callback(err);
+    }
+    collection.findOne({
+      id: id
+    }, function(err, doc) {
+      if (err) {
+        callback(err, null);
+      }
+      var modules = new Modules(doc.id, doc.content, doc.date);
+      callback(null, modules);
+    });
+  });
+};
