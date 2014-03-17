de90cd94a46728b4cc209de327882280eca6f5e4
abysmli <abysmli@gmail.com>
2014-03-17 20:07:24 +0100

    server performence test

--- easy_click_back_end/cluster.js
+++ easy_click_back_end/cluster.js
@@ -5,15 +5,23 @@ var numCPUs = os.cpus().length;
 
 var workers = {};
 if (cluster.isMaster) {
-	cluster.on('death',function (worker) {
-		delete workers[worker.pid];
-		worker = cluster.fork();
-		workers[worker.pid] = worker;
-	});
+	console.log('Master start...');
 	for (var i = 0; i<numCPUs; i++) {
 		var worker = cluster.fork();
 		workers[worker.pid] = worker;
 	}
+	cluster.on('death',function (worker) {
+		delete workers[worker.pid];
+		worker = cluster.fork();
+		workers[worker.pid] = worker;
+		console.log('Death: Worker '+worker.process.pid+' died');
+	});
+	cluster.on('listening', function (worker, address){
+		console.log('listening: Worker '+worker.process.pid+', Address: '+address.address+':'+address.port);
+	});
+	cluster.on('exit', function (worker, code, signal){
+		console.log('Exit: Worker '+worker.process.pid+' died');
+	});
 } else {
 	var app = require('./app');
 	app.listen(80);
