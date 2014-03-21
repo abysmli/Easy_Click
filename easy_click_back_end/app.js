46846c391909b723b9a2b9648e73de684684bdeb
abysmli <abysmli@gmail.com>
2014-03-21 18:16:36 +0100

    fix connect bug

--- easy_click_back_end/app.js
+++ easy_click_back_end/app.js
@@ -19,7 +19,7 @@ app.configure(function() {
   app.use(partials());
   app.use(express.json({limit: '50mb'}));
   app.use(express.urlencoded({limit: '50mb'}));
-  app.use(express.bodyParser({limit: '50mb'}));
+  app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.logger({stream: accessLogfile}));
